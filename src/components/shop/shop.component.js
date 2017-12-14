42f489805c4a72957bb8d37a0ecfedba82a3ed1f
vyaron <vyaron@gmail.com>
2017-12-14 13:31:06 +0200

    use const

--- src/components/shop/shop.component.js
+++ src/components/shop/shop.component.js
@@ -1,11 +1,12 @@
 import {mapGetters, mapMutations} from 'vuex';
+import {GET_PRODUCTS} from '../../modules/shop/shop.module';
 import {ADD_TO_CART, UPDATE_CART} from '../../modules/cart/cart.module';
 
 import Product from '../product';
 
 export default {
   created () {
-    this.$store.dispatch('getProducts');
+    this.$store.dispatch(GET_PRODUCTS);
   },
   methods   : {
     whenQuantityChanges({product, quantity}){
